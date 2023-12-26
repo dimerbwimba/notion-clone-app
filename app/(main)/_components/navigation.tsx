0f96c5ef659d76f7efee57f5accc4553cb130e90
dimerbwimba <dimerbwimba@gmail.com>
2023-12-26 19:45:06 +0100

    Finish add documents & sub-documents

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,10 +6,11 @@ import { usePathname } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./user-item";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import Item from "./item";
 import { toast } from "sonner";
+import DocumentList from "./document-list";
 
 const Navigation = () => {
     const pathname = usePathname()
@@ -21,7 +22,6 @@ const Navigation = () => {
     const [isCollapsed, setIsCollapsed] = useState(false)
     
 
-    const documents = useQuery(api.documents.get)
     const create = useMutation(api.documents.create)
 
     useEffect(()=>{
@@ -144,10 +144,7 @@ const Navigation = () => {
             </div>
 
             <div className="mt-4">
-             
-                {documents?.map((document)=>(
-                  <p key={document._id}>{document.title}</p>
-                ))}
+             <DocumentList/>
             </div>
             <div 
                 onMouseDown={handleMouseDown} 
